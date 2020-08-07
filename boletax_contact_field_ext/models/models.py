0c23d50b9108f9aed37e8c50c167a086f57743c1
julioisaza <julio.isaza@gmail.com>
2020-08-06 23:23:15 -0400

    Update models.py

--- boletax_contact_field_ext/models/models.py
+++ boletax_contact_field_ext/models/models.py
@@ -222,6 +222,7 @@ class ContactExtension(models.Model):
         queue_url = "https://sqs.us-east-1.amazonaws.com/244396393484/chl_synccompanyinfo_{0}".format(pos_obj.x_enviroment)
         
         # Send message to SQS queue
+        event=[]
         event["body"] = {
                     "fiscal_id":pos_obj.document_number,
                     "CountryCode":'chl',
